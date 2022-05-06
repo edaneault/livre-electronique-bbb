ff7c5c28c39fc5334d0a0246b36e61959ad1189e
Hugo <>
2022-05-06 10:11:55 -0400

    Récupération d'un fichier text de gutenberg.org puis affichage de ce dernier sur une page html basique à l'aide d'un server Flask

--- UIgutenberg/app.py
+++ UIgutenberg/app.py
@@ -1,7 +1,5 @@
 from flask import Flask 
 from flask import render_template
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -9,20 +7,4 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
 
-    
-
-    
-
-    with urlopen("https://gutenberg.org/files/2701/2701-h/2701-h.htm") as response:
-        body = response.read()
-
-    # Parse the html file
-    soup = BeautifulSoup(body, 'html.parser')
-
-    # Format the parsed html file
-    strhtm = soup.getText()
-
-
-
-
-    return render_template('index.html', Page_title='UIGutenberg', Book_title=strhtm, code=body)
+    return render_template('index.html', Page_title='UI Gutenberg', Grand_title='Click below to access the ebook')
