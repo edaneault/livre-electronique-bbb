06820dd12eb3c4528c1757b89a28b346ab2bf9b4
Émile <edaneault@cmaisonneuve.qc.ca>
2022-06-09 16:17:56 +0000

    Adapter code python bouton aux GPIOs utilisés et corriger fonctionnement

--- codes/scripts/code_bouton.py
+++ codes/scripts/code_bouton.py
@@ -1,74 +1,39 @@
-
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-B_Ar = "P8_14" 
-B_Go = "P8_16"
-B_Av = "P8_18"
+# Variables globales pour l'état de chacun des boutons
+etatArriere = 0
+etatGo = 0
+etatAvant = 0
 
-x = ""
-DEL_verte = "P9_21"
-flag = 0
-flag2 = 0
-flag3 = 0
+# Direction des GPIOs
+pinArriere = "P9_11" 
+pinGo = "P9_13"
+pinAvant = "P9_15"
+GPIO.setup(pinAvant, GPIO.IN)
+GPIO.setup(pinGo, GPIO.IN)
+GPIO.setup(pinArriere, GPIO.IN)
 
-Ar = 0
-Go = 0
-Av = 0
+# Fonctions callback pour quand un bouton est pesé
+def callbackBoutonArriere(self):
+    global etatArriere
+    etatArriere = 1
+def callbackBoutonGo(self):
+    global etatGo
+    etatGo = 1
+def callbackBoutonAvant(self):
+    global etatAvant
+    etatAvant = 1
 
-GPIO.setup(B_Av, GPIO.IN)
-GPIO.setup(B_Go, GPIO.IN)
-GPIO.setup(B_Ar, GPIO.IN)
-GPIO.setup(DEL_verte, GPIO.OUT)
-
-def Surprise(self):
-        global flag
-        global flag2
-        global flag3
-
-        if GPIO.input(B_Ar):
-                flag = 1
-        else:
-                flag = 0
-        if GPIO.input(B_Ar):
-                flag2 = 1
-        else:
-                flag2 = 0
-        if GPIO.input(B_Ar):
-                flag3 = 1
-        else:
-                flag3 = 0
-
-
-GPIO.add_event_detect(B_Ar, GPIO.FALLING, callback=Surprise, bouncetime=150)
-GPIO.add_event_detect(B_Go, GPIO.FALLING, callback=Surprise, bouncetime=150)
-GPIO.add_event_detect(B_Av, GPIO.FALLING, callback=Surprise, bouncetime=150)
+# Configuration des callbacks
+GPIO.add_event_detect(pinArriere, GPIO.FALLING, callback=callbackBoutonArriere, bouncetime=150)
+GPIO.add_event_detect(pinGo,      GPIO.FALLING, callback=callbackBoutonGo,      bouncetime=150)
+GPIO.add_event_detect(pinAvant,   GPIO.FALLING, callback=callbackBoutonAvant,   bouncetime=150)
 
 while True:
-
-        for i in range(1000):
-                if flag == 1: 
-                        Ar = 1
-                        print("S{:04d}".format(i)+"Ar{}".format(Ar) +"G{}".format(Go)+"Av{}".format(Av))
-                        time.sleep(1)
-                elif flag == 0:
-                        Ar = 0
-                        print("S{:04d}".format(i)+"Ar{}".format(Ar) +"G{}".format(Go)+"Av{}".format(Av))
-                        time.sleep(1)
-                if flag2 == 1: 
-                        Go = 1
-                        print("S{:04d}".format(i)+"Ar{}".format(Ar) +"G{}".format(Go)+"Av{}".format(Av))
-                        time.sleep(1)
-                elif flag2 == 0:
-                        Go = 0
-                        print("S{:04d}".format(i)+"Ar{}".format(Ar) +"G{}".format(Go)+"Av{}".format(Av))
-                        time.sleep(1)
-                if flag3 == 1: 
-                        Av = 1
-                        print("S{:04d}".format(i)+"Ar{}".format(Ar) +"G{}".format(Go)+"Av{}".format(Av))
-                        time.sleep(1)
-                elif flag3 == 0:
-                        Av = 0
-                        print("S{:04d}".format(i)+"Ar{}".format(Ar) +"G{}".format(Go)+"Av{}".format(Av))
-                        time.sleep(1)
-
+    for i in range(1000):
+        print("S{:04d}".format(i)+"Ar{}".format(etatArriere) +"Go{}".format(etatGo)+"Av{}".format(etatAvant))
+        etatArriere = 0
+        etatGo = 0
+        etatAvant = 0
+        time.sleep(0.2)
